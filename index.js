265e34dd69be8169aa53cdda7ef6af6e00387948
github username <github email address>
2021-04-10 20:44:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+let num3 = 12
+let num4 = 8
+let numbers = (5, 10, 15, 20)
+
+let multiply = num1 * num2
+let random = Math.ceil(Math.random() * 1000)
+let mod = num3 % num4
+let max = Math.max(numbers)
